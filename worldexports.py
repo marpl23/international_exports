79ef91bdd681d00e39029a05d731e2c2a6fc475e
Mar Penalva Lozano <160912407+marpl23@users.noreply.github.com>
2024-09-06 21:54:50 +0200

    worldexports.py

--- worldexports.py
+++ worldexports.py
@@ -25,7 +25,7 @@ from sklearn.linear_model import LinearRegression
 st.set_page_config(page_title='International Trade Project', layout='wide', page_icon='🌎')
 
 # --------------------IMG--------------------------
-inttrade_path = r'img\INTERNATIONALTRADE.png'
+inttrade_path = r'img/INTERNATIONALTRADE.png'
 
 # ------------------------DATA----------------------------
 KAGGLE_USERNAME = "marpenalva"
